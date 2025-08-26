d961cc1ce7bdcc5c2b80320f8cbcadfa7d2f760c
RodrigoDEV <rocapucho123123@gmail.com>
2025-08-26 16:22:35 -0300

    atualizando nomes de com

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ load_dotenv()
 baud = int(os.getenv("BAUD_RATE", 9600))
 
 # Pegar as portas
-com1 = os.getenv("COM1")
-com2 = os.getenv("COM2")
-com3 = os.getenv("COM3")
-com4 = os.getenv("COM4")
+COM_A = os.getenv("COM_A")
+COM_B = os.getenv("COM_B")
+COM_C = os.getenv("COM_C")
+COM_D = os.getenv("COM_D")
 
 # Conectar em cada porta
-s1 = serial.Serial(com1, baud, timeout=1); time.sleep(2)
-s2 = serial.Serial(com2, baud, timeout=1); time.sleep(2)
-s3 = serial.Serial(com3, baud, timeout=1); time.sleep(2)
-s4 = serial.Serial(com4, baud, timeout=1); time.sleep(2)
+s1 = serial.Serial(COM_A, baud, timeout=1); time.sleep(2)
+s2 = serial.Serial(COM_B, baud, timeout=1); time.sleep(2)
+s3 = serial.Serial(COM_C, baud, timeout=1); time.sleep(2)
+s4 = serial.Serial(COM_D, baud, timeout=1); time.sleep(2)
 
 # Enviar "hello world" em cada
 msg = "hello world\n".encode("utf-8")
